3ae98db2bf66f655b0d957772a21267426ea90ee
CW-B-W <cw.brad.wang@gmail.com>
2021-12-15 01:02:00 +0800

    Simple bitplane conversion

---
+++ DLP-Compression/DLP-Compression/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace DLP_Compression
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toBitplansToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(608, 684);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.toBitplansToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(982, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // toBitplansToolStripMenuItem
+            // 
+            this.toBitplansToolStripMenuItem.Name = "toBitplansToolStripMenuItem";
+            this.toBitplansToolStripMenuItem.Size = new System.Drawing.Size(93, 24);
+            this.toBitplansToolStripMenuItem.Text = "ToBitplans";
+            this.toBitplansToolStripMenuItem.Click += new System.EventHandler(this.toBitplansToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 953);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toBitplansToolStripMenuItem;
+    }
+}
+
---
+++ DLP-Compression/DLP-Compression/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace DLP_Compression
+{
+    public partial class Form1 : Form
+    {
+        Bitmap gBitmap = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool[,,,] ConvertToBitplanes(Bitmap bmp)
+        {
+            Color[,] colors = TQ.BitmapFasterIO.LoadFromBitmap(bmp);
+            bool[,,,] bitplanes = new bool[8, 3, bmp.Width, bmp.Height];
+            for (int t = 0; t < 8; ++t)
+            {
+                for (int x = 0; x < bmp.Width; ++x)
+                {
+                    for (int y = 0; y < bmp.Height; ++y)
+                    {
+                        byte R_val = colors[x, y].R;
+                        byte G_val = colors[x, y].G;
+                        byte B_val = colors[x, y].B;
+
+                        int threshold = 1 << t;
+                        bitplanes[t, 0, x, y] = (int)R_val > threshold;
+                        bitplanes[t, 1, x, y] = (int)G_val > threshold;
+                        bitplanes[t, 2, x, y] = (int)B_val > threshold;
+                    }
+                }
+            }
+
+            return bitplanes;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (gBitmap != null)
+            {
+                gBitmap.Dispose();
+            }
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.ShowDialog();
+                gBitmap = (Bitmap)Image.FromFile(ofd.FileName);
+
+                pictureBox1.Image = gBitmap;
+
+                //Console.WriteLine(gBitmap.PixelFormat);
+                //Color[,] colors = TQ.BitmapFasterIO.LoadFromBitmap((Bitmap)gBitmap);
+                //for (int i = 0; i < colors.GetLength(0); ++i)
+                //{
+                //    for (int j = 0; j < 100; ++j)
+                //        colors[i, j] = Color.Black;
+                //}
+                //Bitmap bmp_save = new Bitmap(gBitmap.Width, gBitmap.Height, gBitmap.PixelFormat);
+                //TQ.BitmapFasterIO.SaveToBitmap(bmp_save, colors);
+                //bmp_save.Save(ofd.FileName + "save.bmp");
+            }
+        }
+
+        private void toBitplansToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bool[,,,] bitplanes = ConvertToBitplanes(gBitmap);
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                fbd.ShowDialog();
+                for (int t = 0; t < 8; ++t)
+                {
+                    for (int c = 0; c < 3; ++c)
+                    {
+                        string color_name = null;
+                        Bitmap bmp = new Bitmap(bitplanes.GetLength(2), bitplanes.GetLength(3), System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+                        Color[,] colors = new Color[bmp.Width, bmp.Height];
+                        if (c == 0)
+                        {
+                            color_name = "R";
+                            for (int i = 0; i < colors.GetLength(0); ++i)
+                            {
+                                for (int j = 0; j < colors.GetLength(1); ++j)
+                                {
+                                    if (bitplanes[t, c, i, j])
+                                        colors[i, j] = Color.Red;
+                                    else
+                                        colors[i, j] = Color.White;
+                                }
+                            }
+                        }
+                        else if (c == 1)
+                        {
+                            color_name = "G";
+                            for (int i = 0; i < colors.GetLength(0); ++i)
+                            {
+                                for (int j = 0; j < colors.GetLength(1); ++j)
+                                {
+                                    if (bitplanes[t, c, i, j])
+                                        colors[i, j] = Color.Green;
+                                    else
+                                        colors[i, j] = Color.White;
+                                }
+                            }
+                        }
+                        else if (c == 2)
+                        {
+                            color_name = "B";
+                            for (int i = 0; i < colors.GetLength(0); ++i)
+                            {
+                                for (int j = 0; j < colors.GetLength(1); ++j)
+                                {
+                                    if (bitplanes[t, c, i, j])
+                                        colors[i, j] = Color.Blue;
+                                    else
+                                        colors[i, j] = Color.White;
+                                }
+                            }
+                        }
+                        TQ.BitmapFasterIO.SaveToBitmap(bmp, colors);
+                        string savepath = fbd.SelectedPath + @"\bitplane_" + color_name + "_" + t.ToString() + ".bmp";
+                        bmp.Save(savepath);
+                    }
+                }
+            }
+        }
+    }
+}
